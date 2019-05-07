33ccd7ab01bd886c531fb8b63f586b1fc7347dce
Vladimir Safonkin <vsafonkin@gmail.com>
2019-05-07 12:10:48 +0300

    add main.py fold.py

---
+++ main.py
@@ -0,0 +1,1 @@
+from fold import fold
--- test.py
+++ test.py
@@ -1,18 +1,1 @@
-print('hello')
-print('test')
-
-
-def greeting(name):
-    return 'hello, ' + name
-
-
-def matrix_sum(matrix):
-    return sum(map(sum, matrix))
-
-
-def do_something():
-    return null
-
-
-names = "messi ronaldo salah mbappe".split()
-clubs = "barselona juventus liverpool psg".split()
+print('test.py is run!')
